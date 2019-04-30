c6fcb61a96d5617f9a2d7c4e586ad7e3e91e4d17
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-04-30 15:53:17 -0300

    usign jwt to generate user token

---
+++ src/app/controllers/SessionController.js
@@ -0,0 +1,24 @@
+const User = require('../models/User')
+
+class SessionController {
+  async store (req, res) {
+    try {
+      const { email, password } = req.body
+      const user = await User.findOne({ email })
+
+      if (!user) {
+        return res.status(400).json({ error: 'User not found' })
+      }
+
+      if (!user.compareHash(password)) {
+        return res.status(400).json({ error: 'Password incorrect' })
+      }
+
+      return res.json({ user, token: User.generateToken(user) })
+    } catch (error) {
+      return res.json(error)
+    }
+  }
+}
+
+module.exports = new SessionController()
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+module.exports = {
+  expiresIn: 86400,
+  secret: 'gonode'
+}
