733a2042c3d8f571dc1f151bea740f45596ee03a
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-04-30 16:16:31 -0300

    install require-dir to facilitate imports

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,7 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
-const UserController = require('./app/controllers/UserController')
-const SessionController = require('./app/controllers/SessionController')
+const { UserController, SessionController } = require('./app/controllers')
 
 routes.post('/users', UserController.store)
 routes.post('/sessions', SessionController.store)
