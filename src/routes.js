271297b57371af491a5a720598a95ec40269a574
Diogo Queiroz <diogoqueirozsantos97@gmail.com>
2019-05-03 18:09:13 -0300

    add models validation

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const routes = express.Router()
-
+const validate = require('express-validation')
 const authMiddleware = require('./app/middlewares/auth')
 
 const {
@@ -10,16 +10,18 @@ const {
   PurchaseController
 } = require('./app/controllers')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
+const { Ad, Purchase, Session, User } = require('./app/validators')
+
+routes.post('/users', validate(User), UserController.store)
+routes.post('/sessions', validate(Session), SessionController.store)
 
 routes.use(authMiddleware)
 
 routes.get('/ads', AdController.index)
 routes.get('/ads/:id', AdController.show)
-routes.post('/ads', AdController.store)
-routes.put('/ads/:id', AdController.update)
+routes.post('/ads', validate(Ad), AdController.store)
+routes.put('/ads/:id', validate(Ad), AdController.update)
 routes.delete('/ads/:id', AdController.destroy)
 
-routes.post('/purchase', PurchaseController.store)
+routes.post('/purchase', validate(Purchase), PurchaseController.store)
 module.exports = routes
